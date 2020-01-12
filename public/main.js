fea0e0d44eac074a1b14e3941b156a352e27e21b
Karol Baczewski <braxnu@gmail.com>
2020-01-12 18:34:58 +0000

    odczyt todosow z bazy

--- public/main.js
+++ public/main.js
@@ -3,6 +3,20 @@ const itemListEl = dqs('#item-list')
 const newItemInput = dqs('#new-item-text')
 const addNewItemButton = dqs('#add-new-item')
 
+;(async () => {
+  const response = await fetch('/api/todo')
+  const { itemList } = await response.json()
+
+  itemList.forEach(todoItem => {
+    const newItemEl = document.createElement('li')
+
+    newItemEl.innerHTML = todoItem.text
+    itemListEl.appendChild(newItemEl)
+  })
+
+})();
+
+
 addNewItemButton.addEventListener('click', () => {
   const itemText = newItemInput.value
 
--- server.js
+++ server.js
@@ -14,6 +14,12 @@ const TodoModel = mongoose.model('Todo', {
 app.use('/', express.static('./public'))
 app.use(express.json())
 
+app.get('/api/todo', async (req, res) => {
+  const itemList = await TodoModel.find()
+
+  res.send({itemList})
+})
+
 app.post('/api/todo', async (req, res) => {
   await TodoModel.create(req.body)
   res.send({succes: true})
